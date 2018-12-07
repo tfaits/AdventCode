c0f124aa3e60fc9eb9bfc07647613846f0c64579
tfaits <tfaits@bu.edu>
2018-12-07 20:49:57 +0000

    Update d7q2.py
    Added comments for easier comprehension.

--- d7q2.py
+++ d7q2.py
@@ -1,28 +1,34 @@
 first = []
 second = []
 f = open("D7Q1.txt","r")
+#Read in the list of letters that come first in each line (requirements) and the matching letters that come second (dependents)
 for line in f:
   first.append(line.split()[1])
   second.append(line.split()[7])
 
+#dumb, I know. I should be using ord() or regex or something.
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+#Time left on each worker's current job
 work1 = 0
 work2 = 0
 work3 = 0
 work4 = 0
 work5 = 0
+#The name of each workers job (False if they don't have one)
 elf1 = False
 elf2 = False
 elf3 = False
 elf4 = False
 elf5 = 0
 
+#start the clock
 time = 0
 
-while len(first) > 0:
-    if len(first) == 1:
+while len(first) > 0: #If we're out of orders, we can just add the time for the last step instead of looping through the ticker
+    if len(first) == 1: #Check to see what the last step will be, to know how much time to add once we break the loop
         lastOne = ord(second[0]) - 5
+    #For each worker, see if they're done worknig. If so, pop the step they completed from the lists of orders.
     if elf1 and work1 == 0:
         idx = 0
         while idx < len(first):
@@ -67,7 +73,9 @@ while len(first) > 0:
                 second.pop(idx)
             else:
                 idx += 1
-        elf5 = False             
+        elf5 = False
+    #Now that the workers are reset (if done), check each step to see if it's ready to be worked on.
+    #If so, assign it to the first available worker.
     for letter in range(0,len(alphabet)):
         if alphabet[letter] in first and alphabet[letter] not in second and alphabet[letter] not in [elf1,elf2,elf3,elf4,elf5]:
             if work1 == 0:
@@ -85,6 +93,7 @@ while len(first) > 0:
             elif  work5 == 0:
                 elf5 = alphabet[letter]
                 work5 = 61 + letter
+    #Workers make 1 second of progress and the clock ticks forward by 1 second.
     if work1 > 0:
         work1 -= 1
     if work2 > 0:
@@ -97,4 +106,5 @@ while len(first) > 0:
         work5 -= 1
     time += 1
 
+#Print clock time plus the time the final step will take.
 print time + lastOne
